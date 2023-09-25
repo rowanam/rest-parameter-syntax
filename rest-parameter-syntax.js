d5b5d86e4ac05811bd423caf3eb54c7b3cc7e68e
rowanam <rowanamaas@gmail.com>
2023-09-25 14:27:34 +0000

    Initial commit

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -1,10 +1,51 @@
 /**
- * To run this file in Gitpod, use the 
+ * To run this file in Gitpod, use the
  * command node rest-parameter-syntax.js in the terminal
  */
 
-// Regular function call 
+// Regular function call
+const sumAll = (a, b, c) => a + b + c;
+let sum1 = sumAll(1, 2, 3);
+console.log(sum1);
 
 // Extra arguments are ignored
+let sum2 = sumAll(1, 2, 3, 4, 5, 6);
+console.log("Sum2:", sum2);
 
 // Function using ...rest
+const sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c;
+    for (let i of rest) {
+        sum += i;
+    }
+    
+    return sum;
+}
+
+let sum3 = sumRest(1, 2, 3, 4, 5, 6);
+console.log("Sum3:", sum3);
+
+const sumAllRest = (...rest) => {
+    let sum = 0;
+    for (let i of rest) {
+        sum += i;
+    }
+
+    return sum;
+}
+
+let sum4 = sumAllRest(1, 2, 3, 4, 5, 6);
+console.log("Sum4:", sum4);
+
+// Trying something else
+
+let arr1 = [1, 2, 3];
+let arr2 = arr1;
+console.log("2: Equal,", arr1 == arr2);
+console.log("2: Stricly equal,", arr1 === arr2);
+
+let arr3 = [...arr1];
+console.log("Arr1:", arr1);
+console.log("Arr3:", arr3);
+console.log("3: Equal,", arr1 == arr3);
+console.log("3: Stricly equal,", arr1 === arr3);
